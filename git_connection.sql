f3086b1b027303a8f7114cdce40c317dec88f544
kfukamori <120128560+sfc-gh-kfukamori@users.noreply.github.com>
2026-06-12 04:17:46 +0900

    Update git_connection.sql

--- git_connection.sql
+++ git_connection.sql
@@ -1,10 +1,10 @@
 -- Git連携のため、API統合を作成する
 CREATE OR REPLACE API INTEGRATION git_api_integration
   API_PROVIDER = git_https_api
-  API_ALLOWED_PREFIXES = ('https://github.com/snow-jp-handson-org/')
+  API_ALLOWED_PREFIXES = ('https://github.com/sfc-gh-kfukamori/')
   ENABLED = TRUE;
 
 -- GIT統合の作成
 CREATE OR REPLACE GIT REPOSITORY GIT_INTEGRATION_FOR_HANDSON
   API_INTEGRATION = git_api_integration
-  ORIGIN = 'https://github.com/snow-jp-handson-org/cortex-handson-jp.git';
+  ORIGIN = 'https://github.com/sfc-gh-kfukamori/kumamoto-opendata-snowflake.git';
