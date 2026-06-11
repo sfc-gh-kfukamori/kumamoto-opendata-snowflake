620d803b1ae90cc32a66bc55cc60b9fe6ecd731e
kfukamori <120128560+sfc-gh-kfukamori@users.noreply.github.com>
2026-06-12 04:15:59 +0900

    Create git_connection.sql

---
+++ git_connection.sql
@@ -0,0 +1,10 @@
+-- Git連携のため、API統合を作成する
+CREATE OR REPLACE API INTEGRATION git_api_integration
+  API_PROVIDER = git_https_api
+  API_ALLOWED_PREFIXES = ('https://github.com/snow-jp-handson-org/')
+  ENABLED = TRUE;
+
+-- GIT統合の作成
+CREATE OR REPLACE GIT REPOSITORY GIT_INTEGRATION_FOR_HANDSON
+  API_INTEGRATION = git_api_integration
+  ORIGIN = 'https://github.com/snow-jp-handson-org/cortex-handson-jp.git';
